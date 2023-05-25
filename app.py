7fde03c2e657e59cc57a9ba316ac98a97ddd5156
duraflEx <duraflex@gmail.com>
2023-05-25 22:08:27 +0200

    first commit

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        inputText = request.form["inputText"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_recipe_prompt(inputText),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,7 +22,7 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
+def generate_prompt(inputText):
     return """Suggest three names for an animal that is a superhero.
 
 Animal: Cat
@@ -31,5 +31,15 @@ Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: {}
 Names:""".format(
-        animal.capitalize()
+        inputText.capitalize()
     )
+
+def generate_recipe_prompt(inputText):
+    prompt = "Je bent een verkoper in een online supermarkt en weet een groot aantal recepten die kunnen gemaakt worden met de ingredienten in je winkel.";
+    prompt += "Je geeft een mooie oplijsting van alle ingredienten aan de klant."
+    prompt += "De klant heeft interesse in recepten in het {}";
+    prompt += "Je bent steeds beleeft tegen de klant en antwoord in de Nederlandse taal.";
+
+    prompt.format(inputText.capitalize())
+
+    return prompt;
