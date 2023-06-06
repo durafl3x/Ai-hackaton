b9fc817d0713af3bb65cd2a3d5d6d82d37dec42b
duraflEx <duraflex@gmail.com>
2023-06-06 21:42:41 +0200

    Added UI Changes

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ def index():
         inputText = request.form["inputText"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_recipe_prompt(inputText),
-            temperature=0.6,
+            prompt=generate_shopping_basket_prompt(inputText),
+            max_tokens=1100,
+            temperature=0.75,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -34,11 +35,32 @@ Names:""".format(
         inputText.capitalize()
     )
 
+def generate_shopping_basket_prompt(inputText):
+    prompt = "Je bent een online assistent voor Collect&Go die de klant helpt om zijn boodschappenlijst samen te stellen.";
+    prompt += "Je kan hierin Belgische seizoensartikelen voorstellen aan de klant."
+    prompt += "Je geeft een oplijsting van de verschillende artikelen en prijs per artikel zodanig dat de klant een prijsbewuste keuze kan maken."
+    return prompt;
+
 def generate_recipe_prompt(inputText):
-    prompt = "Je bent een verkoper in een online supermarkt en weet een groot aantal recepten die kunnen gemaakt worden met de ingredienten in je winkel.";
-    prompt += "Je geeft een mooie oplijsting van alle ingredienten aan de klant."
-    prompt += "De klant heeft interesse in recepten in het {}";
-    prompt += "Je bent steeds beleeft tegen de klant en antwoord in de Nederlandse taal.";
+    prompt = "Je bent een verkoper in de online supermarkt Collect&Go en weet een groot aantal recepten die kunnen gemaakt worden met de ingredienten beschikbaar in je winkel.";
+    prompt += "Je doet een suggestie van een recept aan de klant met een leuke titel, de benodigde ingredienten en de nodige stappen om het recept klaar te maken. Probeer dit kort en bondig te houden."
+    prompt += "De klant heeft interesse om het volgende te koken: " + inputText;
+    ##prompt += "Je bent steeds beleeft tegen de klant en antwoord in de Nederlandse taal.";
+    prompt += "\n";
+    ##prompt += "Q: De klant heeft interesse om het volgende klaar te maken: Gezond desert met fruit";
+    ##prompt += "A: Regenboogfruitsla";
+    ##prompt += "Ingredienten: ";
+    ##prompt += "- 75g blauwe bessen";
+    ##prompt += "- 12 blauwe druiven, 12 aardbeien";
+    ##prompt += "- 2 groene kiwi's";
+    ##prompt += "- 2 platerina's";
+    ##prompt += "- 0.25 cavaillon-meloen";
+    ##prompt += "Bereiding (duur 15min):";
+    ##prompt += "1. Schil de kiwi’s en snij in stukjes.";
+    ##prompt += "2. Snij het vruchtvlees van de meloen in blokjes.";
+    ##prompt += "3. Schil de platerina’s en snij in stukken.";
+    ##prompt += "4. Snij de aardbeien in de lengte in 4.";
+    ##prompt += "5. Verdeel de druiven over glazen en vul aan met de blauwe bessen, de kiwi, de platerina en de meloen. Eindig met de aardbeien.";
 
     prompt.format(inputText.capitalize())
 
